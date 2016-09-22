b4b5601f0523368040e98fce9dd02d8a0d0c4a90
Aoi Fukuoka <aoponaopon85746@gmail.com>
2016-09-22 17:04:31 +0900

    Initial Commit

---
+++ XMLModelizer/XMLModelizer.h
@@ -0,0 +1,19 @@
+//
+//  XMLModelizer.h
+//  XMLModelizer
+//
+//  Created by aoponaopon on 2016/09/22.
+//  Copyright © 2016年 aoponapopon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMLModelizer.
+FOUNDATION_EXPORT double XMLModelizerVersionNumber;
+
+//! Project version string for XMLModelizer.
+FOUNDATION_EXPORT const unsigned char XMLModelizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMLModelizer/PublicHeader.h>
+
+
