646d85e9b59224b296d7dcad5b6f2e62b928cb25
Aoi Fukuoka <aoponaopon85746@gmail.com>
2016-09-22 17:31:57 +0900

    Add demo project.

---
+++ Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by aoponaopon on 2016/09/22.
+//  Copyright © 2016年 aoponapopon. All rights reserved.
+//
+
+import UIKit
+import XMLModelizer
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
